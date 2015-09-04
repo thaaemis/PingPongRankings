0bca3c037afc93fd59cbca79ab5d9a434e9b82e3
thaaemis <bfkraus@gmail.com>
2015-09-03 22:05:24 -0400

    typo

--- digestleague.py
+++ digestleague.py
@@ -57,7 +57,7 @@ def getPlayerFile(startdate,path):
     else:
         recentInds = [ i-1 for i,date in enumerate(datesSaved) if date>=startdate ]
         recentInd = -1 if len(recentInds) == 0 else recentInds[0]
-        fileToRetrieve = '/Archive/'+datesSaved[recentInd].isoformat()+'_data.txt'
+        fileToRetrieve = 'Archive/'+datesSaved[recentInd].isoformat()+'_data.txt'
         with open(path+fileToRetrieve,'r') as f:
             saveStr = f.read()
     with open(path+'players.txt','w') as f:
